5f887225f648c1e1ed60617da3cce917979a36b1
REMath <remath.research@gmail.com>
2017-03-28 23:08:40 -0400

    initial commit

---
+++ init.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+unzip isa_counts/counts.zip
---
+++ lookup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+arm=`grep $1 sorted_counts_arm.total | awk '{ sum+=$2} END {print sum}'`
+mips=`grep $1 sorted_counts_mips.total | awk '{ sum+=$2} END {print sum}'`
+x86=`grep $1 sorted_counts_x86.total | awk '{ sum+=$2} END {print sum}'`
+x86_64=`grep $1 sorted_counts_x86_64.total | awk '{ sum+=$2} END {print sum}'`
+echo "ARM: "$arm
+echo "MIPS: "$mips
+echo "x86: "$x86
+echo "x86_64: "$x86_64
+grep -c $1 * | grep -v :0
